6de56e96652b2550db8550e3ecda2997fb46bdb5
rektion <redutilos0@gmail.com>
2020-12-06 21:34:42 +0100

    display results done

--- star_wars/requetes/views.py
+++ star_wars/requetes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from sshtunnel import SSHTunnelForwarder
 from pymongo import MongoClient
 import pprint
+import csv
 
 MONGO_HOST = "devicimongodb028.westeurope.cloudapp.azure.com"
 MONGO_DB = "Project"
@@ -16,37 +17,41 @@ server = SSHTunnelForwarder(
 )
 
 server.start()
-
 client = MongoClient('devicimongodb028.westeurope.cloudapp.azure.com', server.local_bind_port) # server.local_bind_port is assigned local port
 db = client[MONGO_DB]
 
 
 def user(request):
     mycol = db["people"]
+    results = []
     obj = mycol.find({'name' : 'C-3PO'}, {'species':1})
-    print(mycol)
-    j = 0
-    for i in mycol:
-        print(i)
+    with open('requetes\\user.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';')
+        for row in spamreader:
+            results.append(row)
     context = {}
-    results = []  # Put the list of resquest's result to display here
     context["results"] = results
     return render(request, 'requetes/user.html', context)
 
 def analyst(request):
     context = {}
-    results = []  # Put the list of resquest's result to display here
+    results = []
+    with open('requetes\\analyst.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';')
+        for row in spamreader:
+            results.append(row)
     context["results"] = results
     return render(request, 'requetes/analyst.html', context)
 
 def admin(request):
     context = {}
-    results = []  # Put the list of resquest's result to display here
+    results = []
+    with open('requetes\\admin.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';')
+        for row in spamreader:
+            results.append(row)
     context["results"] = results
     return render(request, 'requetes/admin.html', context)
 
 def home(request):
-    context = {}
-    results = []  # Put the list of resquest's result to display here
-    context["results"] = results
-    return render(request, 'requetes/home.html', context)
+    return render(request, 'requetes/home.html')
