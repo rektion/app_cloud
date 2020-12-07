af752dde63194454e6b339dce459e1e725f05da9
rektion <redutilos0@gmail.com>
2020-12-07 13:46:30 +0100

    merge done for real

--- star_wars/requetes/views.py
+++ star_wars/requetes/views.py
@@ -1,37 +1,5 @@
 from django.shortcuts import render
 from pymongo import MongoClient
-<<<<<<< HEAD
-import pprint
-import csv
-
-MONGO_HOST = "devicimongodb028.westeurope.cloudapp.azure.com"
-MONGO_DB = "Project"
-MONGO_USER = "administrateur"
-MONGO_PASS = "fcwP6h3H"
-
-server = SSHTunnelForwarder(
-    MONGO_HOST,
-    ssh_username=MONGO_USER,
-    ssh_password=MONGO_PASS,
-    remote_bind_address=('127.0.0.1', 30000)
-)
-
-server.start()
-client = MongoClient('devicimongodb028.westeurope.cloudapp.azure.com', server.local_bind_port) # server.local_bind_port is assigned local port
-db = client[MONGO_DB]
-
-
-def user(request):
-    mycol = db["people"]
-    results = []
-    obj = mycol.find({'name' : 'C-3PO'}, {'species':1})
-    with open('requetes\\user.csv') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=';')
-        for row in spamreader:
-            results.append(row)
-    context = {}
-    context["results"] = results
-=======
 from ssh_pymongo import MongoSession
 import csv
 
@@ -66,7 +34,6 @@ def user(request):
         ret.append(row)
     context = {}
     context["results"] = ret
->>>>>>> merge_HTML
     return render(request, 'requetes/user.html', context)
 
 def analyst(request):
